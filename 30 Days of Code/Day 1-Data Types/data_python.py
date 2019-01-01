3913abf636f8c070770982eb6be2b051df0e4713
Chris Sworen aka C§ <csworen@users.noreply.github.com>
2019-01-01 10:35:01 -0800

    correct delineation
    between py2 and py3

---
+++ 30 Days of Code/Day 1-Data Types/data_python.py
@@ -0,0 +1,6 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+print(str(i + int(input())) + "\n" + str(d + float(input())) + "\n" + s + raw_input())
+# Python2 does not require () around the statement in the above `print`
--- 30 Days of Code/Day 1-Data Types/data_python2.py
+++
@@ -1,5 +0,0 @@
-i = 4
-d = 4.0
-s = 'HackerRank '
-
-print str(i + int(input())) + "\n" + str(d + float(input())) + "\n" + s + raw_input()
