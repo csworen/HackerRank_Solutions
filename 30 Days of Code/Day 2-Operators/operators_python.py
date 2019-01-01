5a1f2a89759a18a32504ec4e463a2efda540c611
Chris Sworen aka C§ <csworen@users.noreply.github.com>
2019-01-01 10:36:25 -0800

    correct delineation
    between py2 and py3

--- 30 Days of Code/Day 2-Operators/operators_python.py
+++ 30 Days of Code/Day 2-Operators/operators_python.py
@@ -5,11 +5,10 @@ import os
 import random
 import re
 import sys
-import future
 
 def solve(meal_cost, tip_percent, tax_percent):
     print(int(round(meal_cost + meal_cost*tip_percent/100 + meal_cost*tax_percent/100)))
-	# for Python3, remove `import future`
+	# Python2 does not require () around the statement in the above `print`
 
 if __name__ == '__main__':
     meal_cost = float(raw_input())
